148e06ae453ca30de8bb07bd857aac0144f9411c
Cody <cody@codywhitby.net>
2016-10-12 10:15:39 -0400

    thank you text

--- library/admin.php
+++ library/admin.php
@@ -28,20 +28,20 @@ Digging into WP - http://digwp.com/2010/10/customize-wordpress-dashboard/
 function disable_default_dashboard_widgets() {
 	global $wp_meta_boxes;
 	// unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);    // Right Now Widget
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);        // Activity Widget
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']); // Comments Widget
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);  // Incoming Links Widget
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);         // Plugins Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity'] );        // Activity Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments'] ); // Comments Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links'] );  // Incoming Links Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins'] );         // Plugins Widget
 
 	// unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);    // Quick Press Widget
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);     // Recent Drafts Widget
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);           //
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);         //
+	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts'] );     // Recent Drafts Widget
+	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_primary'] );           //
+	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary'] );         //
 
 	// remove plugin dashboard boxes
-	unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);           // Yoast's SEO Plugin Widget
-	unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']);        // Gravity Forms Plugin Widget
-	unset($wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now']);   // bbPress Plugin Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget'] );           // Yoast's SEO Plugin Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard'] );        // Gravity Forms Plugin Widget
+	unset( $wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now'] );   // bbPress Plugin Widget
 
 	/*
 	have more plugin widgets you'd like to remove?
@@ -90,10 +90,12 @@ function bones_login_css() {
 }
 
 // changing the logo link from wordpress.org to your site
-function bones_login_url() {  return home_url(); }
+function bones_login_url() {
+  return home_url(); }
 
 // changing the alt text on the logo to show your site name
-function bones_login_title() { return get_option( 'blogname' ); }
+function bones_login_title() {
+ return get_option( 'blogname' ); }
 
 // calling it only on the login page
 add_action( 'login_enqueue_scripts', 'bones_login_css', 10 );
@@ -112,7 +114,7 @@ you like.
 
 // Custom Backend Footer
 function bones_custom_admin_footer() {
-	_e( '<span id="footer-thankyou">Developed by <a href="http://yoursite.com" target="_blank">Your Site Name</a></span>. Built using <a href="http://themble.com/bones" target="_blank">Bones</a>.', 'bonestheme' );
+	_e( '<span id="footer-thankyou">Built by the VCUarts Communications Office ❤️', 'bonestheme' );
 }
 
 // adding it to the admin area
